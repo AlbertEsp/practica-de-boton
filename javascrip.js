ab3e9677f7b6c00f32b44f55cc01f4bedb4b4312
Alberto <fuamx15@gmail.com>
2023-01-21 20:57:11 -0600

    ahora la trancision del boton de inicio es mas suave, asi mismo al pocisionar el cursor sobre el boton el efecto de agrandado es mas suabe tambien

--- javascrip.js
+++ javascrip.js
@@ -1,3 +1,13 @@
-document.getElementById("boton-inicio").addEventListener("click",function(){
-    window.scroll(0,0);
+// document.getElementById("boton-inicio").addEventListener("click",function(){
+//     window.scroll(0,0);
+// });
+document.getElementById("boton-inicio").addEventListener("click", function(){
+    var i = window.pageYOffset;
+    var intervalId = setInterval(function(){
+        window.scrollTo(0, i);
+        i -= 50; 
+        if (i <= 0) {
+            clearInterval(intervalId);
+        }
+    }, 20);
 });
