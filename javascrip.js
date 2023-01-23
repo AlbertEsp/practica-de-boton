444e0c33bc332ac9905fb835678a71a45686673d
Alberto <fuamx15@gmail.com>
2023-01-22 18:35:02 -0600

    ahora las imagenes son enlaces a paginas externas para mas informacion del tema. el efecto del boton de inicio ahora es mas suve.

--- javascrip.js
+++ javascrip.js
@@ -1,11 +1,11 @@
 // document.getElementById("boton-inicio").addEventListener("click",function(){
 //     window.scroll(0,0);
-// });
+// }); "todo esto era el codigo antes de mejorar el boton ahora con el siguiente codigo queda mejor el efecto scroll"
 document.getElementById("boton-inicio").addEventListener("click", function(){
     var i = window.pageYOffset;
     var intervalId = setInterval(function(){
         window.scrollTo(0, i);
-        i -= 50; 
+        i -= 25; 
         if (i <= 0) {
             clearInterval(intervalId);
         }
